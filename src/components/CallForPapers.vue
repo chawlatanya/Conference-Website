f2b05103fcc77db38c1949a09915cbf1f1216c13
ashita <ashita.kumar@tothenew.com>
2020-04-22 18:16:23 +0530

    commiting two pages

--- src/components/CallForPapers.vue
+++ src/components/CallForPapers.vue
@@ -14,30 +14,46 @@
         </p>
        <div class="container">
          <div class="row">
-            <div class="col-sm-4">
+            <div class="col-sm-6" id="col1">
                 TRACK 1
                     <ul class="list-group">
                             <li class="list-group-item list-group-item-light">> Speech Analysis, Representation and Models</li>
                             <li class="list-group-item list-group-item-secondary">> Spoken Language Recognition and Understanding</li>
-                            <li class="list-group-item list-group-item-dark">Speech-to-Speech Translation</li>
-                            <li class="list-group-item list-group-item-primary">Computer Vision and Speech Recognition</li>
-                            <li class="list-group-item list-group-item-info">Speech-to-Speech Translation</li>
-                            <li class="list-group-item list-group-item-success">Affective Speech Recognition, Interpretation and Synthesis</li>
-                            <li class="list-group-item list-group-item-warning">Speech Enhancement</li>
-                            <li class="list-group-item list-group-item-danger">Speech and Language Resources - Data Collection, Transcription and Annotation</li>
+                            <li class="list-group-item list-group-item-dark">> Speech-to-Speech Translation</li>
+                            <li class="list-group-item list-group-item-primary">> Computer Vision and Speech Recognition</li>
+                            <li class="list-group-item list-group-item-info">> Speech-to-Speech Translation</li>
+                            <li class="list-group-item list-group-item-success">> Affective Speech Recognition, Interpretation and Synthesis</li>
+                            <li class="list-group-item list-group-item-warning">> Speech Enhancement</li>
+                            <li class="list-group-item list-group-item-danger">> Speech and Language Resources - Data Collection, Transcription and Annotation</li>
+                            <li class="list-group-item list-group-item-light">> Speech Interface Design and Human Factors Engineering</li>
+                            <li class="list-group-item list-group-item-secondary">> Computational Resource Constrained Speech</li>
+                            <li class="list-group-item list-group-item-dark">> Spoken Dialogue Systems</li>
+                            <li class="list-group-item list-group-item-primary">> Human Computer Interaction</li>
+                            <li class="list-group-item list-group-item-info">>  Assistive Technologies</li>
+                            <li class="list-group-item list-group-item-success">> Forensic Speech Investigations and Security Systems</li>
+                            <li class="list-group-item list-group-item-warning">> Speech Enhancement</li>
+                            <li class="list-group-item list-group-item-danger">> Speech and Language Resources - Data Collection, Transcription and Annotation</li>                 
                     </ul>
             </div>
-            <div class="col-sm-4"></div>
-            <div class="col-sm-4">TRACK 2
+            
+            <div class="col-sm-6" id="col1">TRACK 2
                 <ul class="list-group">
-                            <li class="list-group-item list-group-item-light">> Speech Analysis, Representation and Models</li>
+                             <li class="list-group-item list-group-item-light">> Speech Analysis, Representation and Models</li>
                             <li class="list-group-item list-group-item-secondary">> Spoken Language Recognition and Understanding</li>
-                            <li class="list-group-item list-group-item-dark">Speech-to-Speech Translation</li>
-                            <li class="list-group-item list-group-item-primary">Computer Vision and Speech Recognition</li>
-                            <li class="list-group-item list-group-item-info">Speech-to-Speech Translation</li>
-                            <li class="list-group-item list-group-item-success">Affective Speech Recognition, Interpretation and Synthesis</li>
-                            <li class="list-group-item list-group-item-warning">Speech Enhancement</li>
-                            <li class="list-group-item list-group-item-danger">Speech and Language Resources - Data Collection, Transcription and Annotation</li>
+                            <li class="list-group-item list-group-item-dark">> Speech-to-Speech Translation</li>
+                            <li class="list-group-item list-group-item-primary">> Computer Vision and Speech Recognition</li>
+                            <li class="list-group-item list-group-item-info">> Speech-to-Speech Translation</li>
+                            <li class="list-group-item list-group-item-success">> Affective Speech Recognition, Interpretation and Synthesis</li>
+                            <li class="list-group-item list-group-item-warning">> Speech Enhancement</li>
+                            <li class="list-group-item list-group-item-danger">> Speech and Language Resources - Data Collection, Transcription and Annotation</li>
+                            <li class="list-group-item list-group-item-light">> Speech Interface Design and Human Factors Engineering</li>
+                            <li class="list-group-item list-group-item-secondary">> Computational Resource Constrained Speech</li>
+                            <li class="list-group-item list-group-item-dark">> Spoken Dialogue Systems</li>
+                            <li class="list-group-item list-group-item-primary">> Human Computer Interaction</li>
+                            <li class="list-group-item list-group-item-info">>  Assistive Technologies</li>
+                            <li class="list-group-item list-group-item-success">> Forensic Speech Investigations and Security Systems</li>
+                            <li class="list-group-item list-group-item-warning">> Speech Enhancement</li>
+                            <li class="list-group-item list-group-item-danger">> Speech and Language Resources - Data Collection, Transcription and Annotation</li>                 
                     </ul>
             </div>
 
@@ -60,19 +76,34 @@ export default {
 <style scoped>
 h1{
     text-align: center;
-    margin:40px 40px 40px;
+   /* margin:40px 40px 40px;*/
+    color: whitesmoke;
 }
 p{
     align-self: center;
-    margin:40px 40px 40px;   
+   /* margin:40px 40px 40px; */
+    text-indent:centre;
+    color: white;
+
 }
 .CallForPapers{
-  background-image: url("https://media.giphy.com/media/c6W48VCLPF1l8Uu18A/giphy.gif"), url("https://media.giphy.com/media/c6W48VCLPF1l8Uu18A/giphy.gif");
+  background-image: url("https://media.giphy.com/media/fC6BAcnLFF2o/giphy.gif"), url("https://media.giphy.com/media/fC6BAcnLFF2o/giphy.gif");
   background-color: #cccccc;
   background-repeat: no-repeat;
   background-size:cover ;
 }
 .scrollbar-near-moon {
 scrollbar-color: #b490ca #F5F5F5;
+}
+
+#col1{
+    color: white;
+}
+
+.list-group{
+    max-height: 600px;
+    margin-bottom: 10px;
+    overflow:scroll;
+    -webkit-overflow-scrolling: touch;
 }
 </style>
